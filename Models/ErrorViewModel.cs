401561a2064be3fbc6f441af6623aa165ba75bb7
mazetmx <mazen.waleed.161299@gmail.com>
2025-09-02 18:34:32 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcSocialMedia.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
